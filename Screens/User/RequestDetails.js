5aecd598c76b1971f7a0345a4f7cf79df4bbee3f
Alex <uie74795@contiwan.com>
2022-11-26 14:42:45 +0200

    Add calendar

--- Screens/User/RequestDetails.js
+++ Screens/User/RequestDetails.js
@@ -1,14 +1,63 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import CalendarStrip from 'react-native-calendar-strip';
 
-const RequestDetails = () => {
+const RequestDetails = () => {  
+  const today = new Date();
+
+  const setPickupDate = () => {
+
+  }
+
   return (
-    <View>
-      <Text>RequestDetails</Text>
+    <View style={styles.container}>
+      <View style={styles.calendar}>
+        <CalendarStrip
+          style={styles.calendarContainer}
+          calendarHeaderStyle={styles.calendarHeaderStyle}
+          dayContainerStyle={styles.dayContainerStyle}
+          numDaysInWeek={7}
+          highlightDateNameStyle={styles.selectedDate}
+          highlightDateNumberStyle={styles.selectedDate}
+          selectedDate={today}
+          showMonth={true}
+          scrollable={true}
+          onDateSelected={(date) => setPickupDate(date)}
+        />
+      </View>
     </View>
   )
 }
 
 export default RequestDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  calendar: {
+    width: '100%',
+    bottom: 100
+  },
+  calendarContainer: {
+    height: 160,
+    paddingTop: 50,
+    paddingBottom: 10,
+  },
+  calendarHeaderStyle: {
+    color: 'green',
+  },
+  dayContainerStyle: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginHorizontal: 5,
+    width: 50,
+
+  },
+  selectedDate: {
+    color: 'red'
+  },
+})
