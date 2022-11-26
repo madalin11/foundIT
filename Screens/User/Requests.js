597355c6f22462f1f0a21cf8b1401af13689f59a
Andrei Pop <uif05731@contiwan.com>
2022-11-26 16:07:57 +0200

    Create request component and requests list

--- Screens/User/Requests.js
+++ Screens/User/Requests.js
@@ -1,11 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import RequestComponent from '../../Components/RequestComponent';
 
-const Requests = () => {
+
+const Requests = ({ navigation }) => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Requests</Text>
-    </View>
+      <ScrollView>
+        
+
+          <RequestComponent
+            key={1}
+            docName={"buletin"}
+            institutionName={"tibi"}
+            id={29}
+            day={12}
+            mounth={2}
+            year={2010}
+            hour={16}
+            minutes={30}
+            status={"Accepted"}
+          />
+            <View style={{marginBottom:12}}>
+
+            </View>
+          <RequestComponent
+            key={2}
+            docName={"buletin"}
+            institutionName={"tibi"}
+            id={29}
+            day={12}
+            mounth={2}
+            year={2010}
+            hour={16}
+            minutes={30}
+            status={"Accepted"}
+          />
+
+        
+      </ScrollView>
+      <TouchableOpacity
+        raised onPress={() => navigation.navigate("Create request screen")}
+      >
+        <Image source={require('../../assets/create.png')} style={{ alignSelf: 'flex-end', width: 70, height: 70, marginTop: -30, marginRight: 15 }} />
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
