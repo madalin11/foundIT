2b2a026aa2c57c91e49ce41114fb57bd83f0bb77
Andrei Pop <uif05731@contiwan.com>
2022-11-27 13:28:12 +0200

    Add requests on operand

---
+++ Components/OperandRequestComponent.js
@@ -0,0 +1,146 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import colors from '../colors'
+
+
+
+const RequestComponent = ({ docName, institutionName, changeStatus, id, day, mounth, year, hour, minutes, status }) => {
+
+    return (
+        <View key={id} style={{
+            backgroundColor: colors.BEIGE,
+            marginVertical: 10,
+        }}>
+            <View style={styles.container}>
+
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <Text style={styles.dateFormat}>
+                        {day}/{mounth}/{year}
+                    </Text>
+                </View>
+                <View style={{ flex: 1, alignContent: 'center' }}>
+                    <Text style={{
+                        fontFamily: 'Arial',
+                        fontWeight: '400',
+                        fontSize: 20,
+                    }}>
+                        {hour}:{minutes}
+                    </Text>
+                </View>
+                <View style={{
+                    backgroundColor: status == 'Accepted' ? 'green' : (status == 'In progress' ? '#C4C4C4' : (status == 'Rejected' ? 'red' : 'white')),
+                    borderRadius: 20,
+                    top: 1
+                }}>
+                    <Text style={{
+                        marginHorizontal: 10,
+                        marginVertical: 3,
+                        fontFamily: 'Arial',
+                        fontSize: 16,
+                        fontWeight: 'bold',
+                        alignSelf: 'center',
+                        color: 'white'
+                    }}>
+                        {status}
+                    </Text>
+                </View>
+            </View>
+            <View>
+
+                <View style={styles.container}>
+                    <Image source={require('../assets/from_to.png')} style={{ width: 35, height: 35, marginTop: 1 }} resizeMode='contain' />
+                    <View style={{ flexDirection: 'column' }}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={{ marginRight: 3, fontFamily: 'Arial', color: '#B2B2B2', fontSize: 16, fontWeight: '400' }}>
+                                Document:
+                            </Text>
+                            <Text style={{ fontFamily: 'Arial', fontSize: 16, fontWeight: '600' }}>
+                                {docName}
+                            </Text>
+                        </View>
+
+
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={{ marginTop: 4, marginRight: 3, fontFamily: 'Arial', color: '#B2B2B2', fontSize: 16, fontWeight: '400' }}>
+                                Institution:
+                            </Text>
+                            <Text style={{ marginTop: 4, fontFamily: 'Arial', fontSize: 16, fontWeight: '600' }}>
+                                {institutionName}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={styles.container1}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{
+                            flex: 1,
+                            backgroundColor: 'rgba(20, 124, 229, 0.8)',
+                            borderBottomLeftRadius: 10,
+                            paddingTop: 7,
+                            
+                        }}>
+                            <TouchableOpacity >
+                                {/* <Image
+                                    style={{ tintColor: 'blue', alignSelf: 'center', width: 24, height: 24, borderRadius: 50 }}
+                                    source={require('../iconsOurDent/inprogress.png')}
+                                /> */}
+                                <Text style={{ alignSelf: 'center', paddingBottom: 7 }}>
+                                    Accept
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{
+                            flex: 1,
+                            backgroundColor: 'rgba(255, 15, 15, 0.8)',
+                            paddingTop: 7,
+                            borderBottomRightRadius: 10,
+                            
+                        }}>
+                            <TouchableOpacity >
+                                {/* <Image
+                                    style={{ tintColor: 'green', alignSelf: 'center', width: 24, height: 24, borderRadius: 50 }}
+                                    source={require('../iconsOurDent/done.png')}
+                                /> */}
+                                <Text style={{ alignSelf: 'center', paddingBottom: 7 }}>
+                                    Decline
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+
+            </View>
+
+        </View>
+
+    )
+}
+
+export default RequestComponent
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.BLUE,
+        marginHorizontal: 5,
+        paddingHorizontal: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 5,
+    },
+    dateFormat: {
+        fontFamily: 'Arial',
+        fontWeight: '500',
+        fontSize: 20,
+    },
+    container1: {
+        flex: 1,
+        borderBottomLeftRadius: 10,
+        marginHorizontal: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomRightRadius: 10,
+    }
+
+})
--- Screens/Operand/Requests.js
+++ Screens/Operand/Requests.js
@@ -1,14 +1,123 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import { useEffect, useState } from 'react'
+import OperandRequestComponent from '../../Components/OperandRequestComponent';
+import { db } from '../../firebase'
 
-const Requests = () => {
+const Requests = ({ navigation }) => {
+
+  const [appointments, setAppointments] = useState([]);
+  //const temp = auth?.currentUser?.uid;
+
+  async function changeStatus(id, state) {
+
+    await db.collection("appointments").doc(id).update({
+      status: state,
+    })
+  }
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection("appointments")
+      .onSnapshot(snapshot => {
+        setAppointments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data()
+          })))
+      }
+      )
+    return unsubscribe;
+  }, [db])
+
+  function filterZZZ(element) {
+    try {
+      if (element.data.name == '') {
+        return false;
+      }
+      try {
+
+        if (element.data.name.toLowerCase().includes(searchText.toLowerCase()))
+          return true;
+
+      } catch (err) {
+
+      }
+      return false
+    } catch (err) {
+
+    }
+    return true
+  }
+
+  const [requests, setRequest] = useState([
+    {
+      id: 1,
+      documentName: "ID Card",
+      institutionName: "city hall",
+      day: 12,
+      mounth: 12,
+      year: 2010,
+      hour: 16,
+      minutes: 30,
+      status: "Accepted"
+    },
+    {
+      id: 2,
+      documentName: "ID Card",
+      institutionName: "city hall",
+      day: 12,
+      mounth: 12,
+      year: 2010,
+      hour: 16,
+      minutes: 30,
+      status: "In progress"
+    }
+  ])
+
   return (
-    <View>
-      <Text>Requests</Text>
-    </View>
+
+
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.titleContainer}>Requests</Text>
+        <ScrollView>
+          {appointments.filter(filterZZZ).map((item) => {
+            return <OperandRequestComponent
+              key={item.id}
+              docName={item.data.documentName}
+              institutionName={item.data.institutionName}
+              id={item.id}
+              day={item.data.date.slice(9,11)}
+              mounth={11}
+              year={item.data.date.slice(14,18)}
+              hour={item.data.hour.slice(1,3)}
+              minutes={item.data.hour.slice(4,6)}
+              status={item.status}
+              changeStatus={changeStatus}
+            />
+          })}
+
+
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Requests
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-around',
+    padding: 12,
+    //alignItems: 'center',
+    //width: '90%'
+  },
+  titleContainer: {
+    justifyContent: "flex-start",
+    fontSize: 40,
+    fontFamily: "Times New Roman",
+    shadowOpacity: 0.2
+  },
+})
